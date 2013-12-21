0d71a5f9cfdcca8b03b05e77d143020fa48f1c77
Gerardo Fernández <gfcalderon@gmail.com>
2013-12-21 20:26:24 +0100

    Enabled bootstrap front-end framework and i18n

--- src/main/java/com/springapp/spring/config/ApplicationConfig.java
+++ src/main/java/com/springapp/spring/config/ApplicationConfig.java
@@ -1,9 +1,21 @@
 package com.springapp.spring.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 /**
  * Spring application config class
@@ -14,6 +26,42 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan( basePackages = { "com.springapp" } )
 @EnableWebMvc
 @Import( { ThymeleafConfig.class } )
-public class ApplicationConfig {
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers( ResourceHandlerRegistry registry ) {
+        registry.addResourceHandler( "/resources/**" ).addResourceLocations( "/resources/" );
+    }
+
+    @Override
+    public void addInterceptors( InterceptorRegistry registry ) {
+        registry.addInterceptor( localeChangeInterceptor() );
+    }
+
+    @Bean
+    @Qualifier( "localeResolver" )
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale( new Locale( "es" ) );
+        return localeResolver;
+    }
+
+    @Bean
+    @Qualifier( "localeChangeInterceptor" )
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName( "lang" );
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    @Qualifier( "messageSource" )
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename( "/WEB-INF/i18n/springappTexts" );
+        messageSource.setDefaultEncoding( "UTF-8" );
+        messageSource.setCacheSeconds( 1 );
+        return messageSource;
+    }
 
 }
